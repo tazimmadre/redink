b5c13a3bf8d2ce737aa4e627bb50beffaba14902
Tazim <tazimmadre5041@gmail.com>
2021-12-10 11:19:12 +0530

    sendgrid implemented

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const db= require("./db");
 const express= require("express");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const app=express();
 app.use(express.json());
 
-// const router= express.Router();
 app.get("/posts", (req, res) => {
   db.execute("SELECT * from blogs")
     .then(([rows, fieldData]) => {
@@ -34,9 +35,30 @@ app.post("/post", (req, res) => {
     const { title, description,author_id } = req.body;
 
   db.execute('INSERT INTO blogs (title,description,author_id) VALUES (?,?,?)',[title,description,author_id])
-    .then(([rows, fieldData]) => {
-      console.log(rows);
-      res.status(201).send({ message: "Success", data: rows });
+    .then(() => {
+      db.execute("SELECT * FROM authors WHERE authors.id!=?", [author_id])
+        .then(([rows]) => {
+          console.log(rows);
+          rows.map(x=>{
+          const msg = {
+            to: `${x.email}`, // Change to your recipient
+            from: "tazimmadre5041@gmail.com", // Change to your verified sender
+            subject: "New Post is HERE",
+            text: "CHeckout the new Post",
+            html: `<strong>CHeckout the new Post ${x.username}</strong>`,
+          };
+          sgMail
+            .send(msg)
+            .then(() => {
+              console.log("Email sent");
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+          })
+        })
+        .catch((e) => console.log(e));
+      res.status(201).send({ message: "Success" });
     })
     .catch((e) => console.log(e));
 });
